431f4c4fabfc5f71ddd335c304d027a12ca803ff
Jose A. Hernando <hernando.joseangel@gmail.com>
2021-04-22 11:09:28 +0200

    sorted tracks

--- clouds.py
+++ clouds.py
@@ -1051,3 +1051,18 @@ def get_function_branches(passes):
         return branches
     
     return get_branches
+
+#
+#  Tools with tracks
+#
+
+
+def sorted_tracks(evt, emin = 0.02):
+    
+    tracks_ids = np.unique(evt.track.values)
+    enes       = np.array([np.sum(evt.ene[evt.track == ik]) for ik in tracks_ids], float)
+    tracks_ids, enes = clouds.sorted_by_energy(tracks_ids, enes)
+    
+    tracks_ids = [id for id, ene in zip(tracks_ids, enes) if ene >= emin]
+
+    return tracks_ids
